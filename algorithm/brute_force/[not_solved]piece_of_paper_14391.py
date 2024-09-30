cdf44e3a5cf9cd6998ba92766bbd5eb91443e0d8
seonjiwon <tommys915@gmail.com>
2024-09-30 22:27:03 +0900

    이름 획일화

--- algorithm/brute_force/[not_solved]piece_of_paper_14391.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-n, m = map(int, sys.stdin.readline().strip().split())
-list = [list(sys.stdin.readline().strip()) for i in range(n)]
--- algorithm/brute_force/[not_solved]sudoku_2580.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-
-sudoku = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(9)]
