15de6c2cf08307e4f3cd80262d253d722ede0817
seonjiwon <tommys915@gmail.com>
2025-02-07 14:43:20 +0900

    .

--- algorithm/graph/[Gold3] 연구소3 17142.py
+++ algorithm/graph/[Gold3] 연구소3 17142.py
@@ -1,15 +1,3 @@
-"""
-7 3
-2 0 0 0 1 1 0
-0 0 1 0 1 2 0
-0 1 1 0 1 0 0
-0 1 0 0 0 0 0
-0 0 0 2 0 1 1
-0 1 0 0 0 0 0
-2 1 0 0 0 0 2
-: 4
-0,0 / 1, 5 / 4, 3
-"""
 import sys
 from collections import deque
 from itertools import combinations
